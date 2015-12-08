9740e093e4b3a56a9b376dd6dfb15099f3dd9878
Julien Bara <julien.bara@insa-lyon.fr>
2015-12-08 11:53:38 +0100

    Preferences

--- TrouvTonResto/app/src/main/java/com/h4404/trouvtonresto/MenuApp.java
+++ TrouvTonResto/app/src/main/java/com/h4404/trouvtonresto/MenuApp.java
@@ -80,20 +80,13 @@ public class MenuApp extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        }/* else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_restaurants) {
+            // Handle the restaurants action
+        } else if (id == R.id.nav_map) {
 
-        }*/ else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_preferences) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } /*else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }*/
-
+        }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
