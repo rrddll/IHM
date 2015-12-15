5134ccdf610f3ea3cb46127a5441dcdc408008ab
Luc Forget <luc.forget@insa-lyon.fr>
2015-12-15 11:34:08 +0100

    Modifications

--- TrouvTonResto/app/src/main/java/com/h4404/trouvtonresto/RestaurantListeAdapter.java
+++ TrouvTonResto/app/src/main/java/com/h4404/trouvtonresto/RestaurantListeAdapter.java
@@ -1,6 +1,7 @@
 package com.h4404.trouvtonresto;
 
 import android.content.Context;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +16,7 @@ import java.util.List;
  */
 public class RestaurantListeAdapter extends ArrayAdapter<Restaurant>{
     private final Context context;
+
     private final List<Restaurant> restos;
     public RestaurantListeAdapter (Context context, List<Restaurant> values) {
         super(context, -1, values);
@@ -30,9 +32,23 @@ public class RestaurantListeAdapter extends ArrayAdapter<Restaurant>{
         TextView titre = (TextView) rowView.findViewById(R.id.titreResto);
         TextView specialite = (TextView) rowView.findViewById(R.id.specialiteResto);
         TextView info = (TextView) rowView.findViewById(R.id.infoChiffreResto);
+        TextView tpsAttente = (TextView) rowView.findViewById(R.id.tpsAttente);
 
+        int tpsAAttendre = restos.get(position).getTempsAttente();
         titre.setText(restos.get(position).getNom());
         specialite.setText(restos.get(position).getSpecialite());
+        if(tpsAAttendre >= 0) {
+            tpsAttente.setText(tpsAAttendre + " min.");
+            if (tpsAAttendre <= 10)
+                tpsAttente.setTextColor(ContextCompat.getColor(context, R.color.valeurOk));
+            else if (tpsAAttendre < 15)
+                tpsAttente.setTextColor(ContextCompat.getColor(context, R.color.valeurMoyenne));
+            else
+                tpsAttente.setTextColor(ContextCompat.getColor(context, R.color.valeurMauvaise));
+        } else {
+            tpsAttente.setText("Attente inconnue");
+        }
+
         int dist = restos.get(position).getDistance();
         String distance;
         if (dist >= 1000) {
