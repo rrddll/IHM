60522b7f3e126179143e129430546e1d684da9b9
tfaure1 <theo.faure@insa-lyon.fr>
2015-12-15 11:51:49 +0100

    splash

--- TrouvTonResto/app/src/main/java/com/h4404/trouvtonresto/CarteFragment.java
+++ TrouvTonResto/app/src/main/java/com/h4404/trouvtonresto/CarteFragment.java
@@ -14,6 +14,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
@@ -38,7 +39,7 @@ public class CarteFragment extends Fragment implements OnMapReadyCallback {
         View view = inflater.inflate(R.layout.fragment_map, container, false);
         edLocation=(EditText)view.findViewById(R.id.rechercheAdresse);
 
-        com.google.android.gms.maps.MapFragment mapFragment = (com.google.android.gms.maps.MapFragment) getFragmentManager()
+        MapFragment mapFragment = (MapFragment)getChildFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         restosName = getResources().getStringArray(R.array.restosName);
